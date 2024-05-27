ac16b72b095bdec5c29c93681d028f1a3a3a51d8
elidaarnaut <elida.arnaut@gmail.com>
2024-05-27 21:24:06 +0200

    Homepage 1 responsive

--- culinary-connect/src/components/Footer.jsx
+++ culinary-connect/src/components/Footer.jsx
@@ -3,10 +3,9 @@ import '../App.css'
 function Footer() {
     return (
         <div className='footer'>
-            <hr></hr>
-            <br />
+            <hr />
             <p>Being developed by students of the International University of Sarajevo</p>
-            <br />
+            
         </div>
     );
 }
